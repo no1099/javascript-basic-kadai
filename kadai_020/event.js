bb94481673ee67d87ea6279f3af081b35f0de56b
TCGsato <tetsu@tcg.co.jp>
2024-01-23 17:33:33 +0900

    kadai_020の模範解答

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,14 @@
 // ボタンがクリックされたら表示する文章を書き換えるプログラム
 const eventBtn = document.getElementById("btn");
-console.log(eventBtn);
 eventBtn.addEventListener("click", ()=> {
     const text = document.getElementById("text");
     text.innerText = "ボタンをクリックしました";
-    console.log(text.value);
+});
+
+// 模範解答
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    text.textContent = "ボタンをクリックしました";
 });
